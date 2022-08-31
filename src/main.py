892584bdcabf5c5da79b90ee7ca422cc6f3c0f58
kacper.wandel <kacper.wandel@capgemini.com>
2022-08-31 15:37:05 +0200

    add logging

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+import datetime as dt
 from sys import argv
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+
+
+logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+def create_rotating_log(path, size=4096):
+    logger = logging.getLogger("rotating_logger")
+    logger.setLevel(logging.INFO)
+    handler = RotatingFileHandler(path, maxBytes=size, backupCount=5)
+    logger.addHandler(handler)
+    return logging.Logger
 
 
 def read_file(filepath):
@@ -19,26 +36,43 @@ def filter_columns(df, column_name, values):
     return df.filter(col(column_name).isin(values))
 
 
+log_filepath = "kommatipara.log"
+
+
 if __name__ == "__main__":
+    log = create_rotating_log(log_filepath)
+    log.info("Starting new run.")
+    if len(argv) != 4:
+        print('Too few or too many arguments!\nReminder: python src/main.py "filepath1" "filepath2" "[list,of,expected,countries]"')
+        log.info("Too few or too many arguments.")
+        exit(1)
     filepath1, filepath2, countries = argv[1:4]
     countries = [e.strip() for e in countries[1:-1].split(',')]
-    print(filepath1, filepath2, countries)
+    log.info("Filepaths:", filepath1, filepath2)
+    log.info("Countries:", countries)
 
     spark = SparkSession.builder.appName("codac").getOrCreate()
     sc = spark.sparkContext
     client_df = read_file(filepath1)
     finance_df = read_file(filepath2)
+    log.info("Files read.")
     
     client_df = client_df.drop("first_name", "last_name")
     finance_df  = finance_df.drop("cc_n")
+    log.info("Columns dropped.")
 
     df = client_df.join(finance_df, "id")
+    log.info("Dataframes joined.")
 
     df = rename_columns(df, {'id': 'client_identifier', 'btc_a': 'bitcoin_address', 'cc_t': 'credit_card_type'})
+    log.info("Columns renamed.")
+
     df = filter_columns(df, "country", countries)
+    log.info("Columns filtered.")
 
-    df.show()
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    target_path = os.path.join(script_path, '..', 'client_data',dt.datetime.strftime(dt.datetime.now(), "%Y%m%d%H%M%S"))
 
-    # client_df.show()
-    # finance_df.show()
-    # client_df.select("country").distinct().show()
+    df.coalesce(1).write.csv(target_path)
+    log.info("File written.")
+    log.info("Run end.")
