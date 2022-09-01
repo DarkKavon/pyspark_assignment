333355ff383c3a49063e771ffb000a449b4efa00
kacper.wandel <kacper.wandel@capgemini.com>
2022-09-01 09:48:26 +0200

    merge transformations

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ def read_file(filepath):
 
 def rename_columns(df, column_names_mapping):
     for k, v in column_names_mapping.items():
-        df = df.withColumnRenamed(k,v)
+        df = df.withColumnRenamed(k, v)
     return df
 
 
@@ -39,7 +39,8 @@ if __name__ == "__main__":
     log = create_rotating_log(log_filepath)
     log.info("Starting new run.")
     if len(argv) != 4:
-        print('Too few or too many arguments!\nReminder: python src/main.py "filepath1" "filepath2" "[list,of,expected,countries]"')
+        print(
+            'Too few or too many arguments!\nReminder: python src/main.py "filepath1" "filepath2" "[list,of,expected,countries]"')
         log.info("Too few or too many arguments.")
         exit(1)
     filepath1, filepath2, countries = argv[1:4]
@@ -52,22 +53,24 @@ if __name__ == "__main__":
     client_df = read_file(filepath1)
     finance_df = read_file(filepath2)
     log.info("Files read.")
-    
+
     client_df = client_df.drop("first_name", "last_name")
-    finance_df  = finance_df.drop("cc_n")
+    finance_df = finance_df.drop("cc_n")
     log.info("Columns dropped.")
 
     df = client_df.join(finance_df, "id")
     log.info("Dataframes joined.")
 
-    df = rename_columns(df, {'id': 'client_identifier', 'btc_a': 'bitcoin_address', 'cc_t': 'credit_card_type'})
+    df = rename_columns(df, {'id': 'client_identifier',
+                        'btc_a': 'bitcoin_address', 'cc_t': 'credit_card_type'})
     log.info("Columns renamed.")
 
     df = filter_columns(df, "country", countries)
     log.info("Columns filtered.")
 
     script_path = os.path.dirname(os.path.realpath(__file__))
-    target_path = os.path.join(script_path, '..', 'client_data',dt.datetime.strftime(dt.datetime.now(), "%Y%m%d%H%M%S"))
+    target_path = os.path.join(script_path, '..', 'client_data', dt.datetime.strftime(
+        dt.datetime.now(), "%Y%m%d%H%M%S"))
 
     df.coalesce(1).write.csv(target_path)
     log.info("File written.")
